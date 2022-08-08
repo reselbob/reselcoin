95c8ab0aa755b91749e9fa52e261b5dc2a45b948
Bob Reselman <bob@CogArtTech.com>
2022-08-08 08:49:03 -0700

    building out ICO logic

--- src/helpers/seeder.ts
+++ src/helpers/seeder.ts
@@ -3,25 +3,33 @@ import { faker } from '@faker-js/faker';
 import {Blockchain} from "../blockchain";
 import {Transaction} from "../transaction";
 import {UnsignedTransaction} from "../interfaces/interfaces";
-
+import {TransactionSigner} from "./transactionSigner";
+import * as elliptic from "elliptic";
+const EC = elliptic.ec;
 
 export class Seeder {
-    private fromAddress:string;
-    private toAddress: string;
-    private miner: string;
+    private readonly miner: string;
+    private seederKeyPrivateKey: string;
+    private seederKeyPublicKey: string;
 
     constructor() {
-        this.toAddress = faker.internet.email();
-        this.fromAddress = faker.internet.email();
+        const ec = new EC('secp256k1');
+        const kp = ec.genKeyPair();
+        this.seederKeyPrivateKey = kp.getPrivate('hex')
+        this.seederKeyPublicKey = kp.getPublic('hex')
         this.miner = 'miner@reselcoin.io';
+
     }
     public seedBlockchain(blockchain: Blockchain, numberOfBlocks: number): Array<Transaction>{
         logger.info(`Seeding blockchain with ${numberOfBlocks} blocks`);
         const transactions = new Array<Transaction>();
         for(let i = 0; i<numberOfBlocks; i++){
-            const transaction = new Transaction(this.fromAddress, this.toAddress, parseInt(faker.random.numeric()));
-            transactions.push(transaction)
-            blockchain.addTransaction(transaction);
+            const utx = this.getRandomUnsignedTransaction();
+            utx.fromAddress = this.seederKeyPublicKey;
+            const signer = new TransactionSigner(utx, this.seederKeyPrivateKey);
+            const signedTx = signer.getSignedTransaction();
+            transactions.push(signedTx.transaction)
+            blockchain.addTransaction(signedTx.transaction);
         }
         blockchain.minePendingTransactions(this.miner);
         return transactions;
